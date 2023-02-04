16225b38cabab9e75fb69161c97830e034dee079
Vaishanvi2303 <105056047+Vaishanvi2303@users.noreply.github.com>
2023-02-04 08:44:45 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,20 @@
+function preload() {
+    ball_touch_paddel = loadSound("ball_touch_paddel.wav");
+    missed = loadSound("missed.wav");
+}
+
+function setup() {
+	canvas = createCanvas(700,600);
+	canvas.parent('canvas');
+	
+	video = createCapture(VIDEO);
+    video.size(700, 600);
+    video.hide();
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    
+}
